4e5c550a33c6bc41bcb6f7828b1c78ad24bcb8a3
Anita Sri Handayani <anita.handa26@gmail.com>
2023-02-17 14:22:47 +0700

    Dynamic pageTitle

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue'),
-      meta: {
-        title: 'Login'
-      }
+const routes = [
+  {
+    path: '/',
+    name: 'admin',
+    component: AdminView,
+    meta: {
+      title: 'Admin'
     }
-  ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue'),
+    meta: {
+      title: 'Login'
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  },
+]
+
+const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes })
+router.beforeEach((to, from, next) => {
+  // Get the page title from the route meta data that we have defined
+  // See further down below for how we setup this data
+  const title = to.meta.title
+
+  //Take the title from the parameters
+  const titleFromParams = to.params.pageTitle;
+  // If the route has a title, set it as the page title of the document/page
+  if (title) {
+    document.title = title
+  }
+  // If we have a title from the params, extend the title with the title
+  // from our params
+  if (titleFromParams) {
+    document.title = `${titleFromParams} - ${document.title}`;
+  }
+  // Continue resolving the route
+  next()
 })
 
 export default router
---
+++ src/views/AdminView.vue
@@ -0,0 +1,3 @@
+<template>
+  ADMIN PAGE
+</template>
